de2bb813052978351bc0febab9c4008c07fd32d1
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-24 22:57:00 +0100

    with mysqld_safe

--- srcs/requirements/mariadb/tools/start_script.sh
+++ srcs/requirements/mariadb/tools/start_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-service mariadb start --bind-address=0.0.0.0
+service mariadb start 
 mariadb < db_script.sql
-tail -500f /dev/null
+sleep 1
+#PID=$!
+#wait $PID
+service mariadb stop
+
+mariadbd-safe --bind-address=0.0.0.0 
+#tail -500f /dev/null
 #--datadir=/var/lib/mysql
