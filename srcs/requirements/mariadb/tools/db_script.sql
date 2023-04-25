fec71aae2b7f57d08b112c632bdaed11a34e7e4e
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-25 23:05:31 +0100

    adding env variables to Db

--- srcs/requirements/mariadb/tools/db_script.sql
+++
@@ -1,8 +0,0 @@
--- Vai executar apenas se não existir enão criar erros
--- IF NOT EXISTS
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'password';
---substituir o mydb pela bd necessária ao wordpress
-GRANT ALL PRIVILEGES ON mydb.* TO 'root'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'%' IDENTIFIED BY 'password';
-CREATE DATABASE mydb;
--- srcs/requirements/mariadb/tools/start_script.sh
+++ srcs/requirements/mariadb/tools/start_script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 service mariadb start 
-mariadb < db_script.sql
+
+mariadb <<EOF
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+--substituir o mydb pela bd necessária ao wordpress
+GRANT ALL PRIVILEGES ON mydb.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
 sleep 1
-#PID=$!
-#wait $PID
+
 service mariadb stop
 
 mariadbd-safe --bind-address=0.0.0.0 
-#tail -500f /dev/null
-#--datadir=/var/lib/mysql
