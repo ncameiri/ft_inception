41c6393a75cb2b17aa7de62c4df2884791402094
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-25 10:15:51 +0100

    verificado o acesso à db sem expor os ports

--- srcs/volume_reset.sh
+++ srcs/volume_reset.sh
@@ -1,5 +1,8 @@
 #cada vez que se faz o up tem de se alterar estas permissões aos volumes para que o sitema consiga utilizar as folders
 #o rm apaga toda a informação
-sudo rm -rf requirements/mariadb/data/*
+#sudo rm -rf requirements/mariadb/data/*
+#sudo rm -rf requirements/wordpress/data/*
 sudo chmod -R 777 requirements/mariadb/data
-sudo  chown -R nuno requirements/mariadb/data
+sudo chown -R nuno requirements/mariadb/data
+sudo chmod -R 777 requirements/wordpress/data
+sudo chown -R nuno requirements/wordpress/data
