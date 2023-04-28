e85c895c35476b8fed63bcc0e4f5fd2681c166d6
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-28 08:08:54 +0100

    funciona mal ainda o php-fpm

--- srcs/requirements/wordpress/tools/start_script.sh
+++ srcs/requirements/wordpress/tools/start_script.sh
@@ -7,8 +7,10 @@ sed -i "s/MYSQL_USER/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
 sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /var/www/wordpress/wp-config.php
 
 
+chown -R nginx:nginx /var/www/*
+chown -R 755 /var/www/*
 #Remove Original config and change for updated
 rm /etc/php/7.4/fpm/pool.d/www.conf
 cp /www.conf /etc/php/7.4/fpm/pool.d/
-
+php-fpm7.4 -F -R
 tail -500f /dev/null
