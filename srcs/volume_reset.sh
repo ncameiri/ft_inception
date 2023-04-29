2fa1d800b575b330db6bed3427618117b2189015
ncameiri <74956154+ncameiri@users.noreply.github.com>
2023-04-29 20:51:51 +0100

    Update volume_reset.sh

--- srcs/volume_reset.sh
+++ srcs/volume_reset.sh
@@ -1,10 +1,8 @@
 #cada vez que se faz o up tem de se alterar estas permissões aos volumes para que o sitema consiga utilizar as folders
 #o rm apaga toda a informação
-sudo rm -rf /home/ncameiri/data/*
-sudo rm -rf /home/ncameiri/data/*
+sudo rm -rf /home/ncameiri/data/wordpress/*
+sudo rm -rf /home/ncameiri/data/mariadb/*
 docker volume rm srcs_volume_mariadb
 docker volume rm srcs_volume_wordpress
-sudo chmod -R 777 /home/ncameiri/data
-sudo chown -R ncameiri:ncameiri /home/ncameiri/data
 sudo chmod -R 777 /home/ncameiri/data
 sudo chown -R ncameiri:ncameiri /home/ncameiri/data
