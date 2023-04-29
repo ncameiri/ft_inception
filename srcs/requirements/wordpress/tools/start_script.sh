c6de30696ad8cf982de16508a5dae7fa32644546
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-29 11:09:54 +0100

    css finnaly working with volume mounted on nginx

--- srcs/requirements/wordpress/tools/start_script.sh
+++ srcs/requirements/wordpress/tools/start_script.sh
@@ -2,13 +2,13 @@ echo "###########################"
 echo "STARTED WORDPRESS CONTAINER"
 echo "###########################"
 
-#sed -i "s/WP_DBNAME/$WP_DBNAME/g" /var/www/wordpress/wp-config.php
-#sed -i "s/MYSQL_USER/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
-#sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /var/www/wordpress/wp-config.php
+sed -i "s/WP_DBNAME/$WP_DBNAME/g" /var/www/wordpress/wp-config.php
+sed -i "s/MYSQL_USER/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
+sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /var/www/wordpress/wp-config.php
 
 
+chmod -R 775 /var/www/
 chown -R www-data:www-data /var/www/
-chown -R 775 /var/www/
 #Remove Original config and change for updated
 mkdir -p /run/php
 touch /run/php/php7.4-fpm.pid
@@ -16,4 +16,4 @@ rm /etc/php/7.4/fpm/pool.d/www.conf
 
 cp /www.conf /etc/php/7.4/fpm/pool.d
 php-fpm7.4 -F -R
-tail -500f /dev/null
+#tail -500f /dev/null
