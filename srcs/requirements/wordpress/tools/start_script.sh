898e5534a3ff0b943edbaea2d9d47a65dbdb618b
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-29 20:40:36 +0100

    only makefile

--- srcs/requirements/wordpress/tools/start_script.sh
+++ srcs/requirements/wordpress/tools/start_script.sh
@@ -1,11 +1,13 @@
 echo "###########################"
 echo "STARTED WORDPRESS CONTAINER"
 echo "###########################"
-
+sleep 2
 sed -i "s/WP_DBNAME/$WP_DBNAME/g" /var/www/wordpress/wp-config.php
+sleep 1
 sed -i "s/MYSQL_USER/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
+sleep 1
 sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /var/www/wordpress/wp-config.php
-
+sleep 2
 
 chmod -R 775 /var/www/
 chown -R www-data:www-data /var/www/
--- srcs/volume_reset.sh
+++ srcs/volume_reset.sh
@@ -1,10 +1,10 @@
 #cada vez que se faz o up tem de se alterar estas permissões aos volumes para que o sitema consiga utilizar as folders
 #o rm apaga toda a informação
-sudo rm -rf requirements/mariadb/data/*
-sudo rm -rf requirements/wordpress/data/*
+sudo rm -rf /home/ncameiri/data/*
+sudo rm -rf /home/ncameiri/data/*
 docker volume rm srcs_volume_mariadb
 docker volume rm srcs_volume_wordpress
-sudo chmod -R 777 requirements/mariadb/data
-sudo chown -R nuno requirements/mariadb/data
-sudo chmod -R 777 requirements/wordpress/data
-sudo chown -R nuno requirements/wordpress/data
+sudo chmod -R 777 /home/ncameiri/data
+sudo chown -R ncameiri:ncameiri /home/ncameiri/data
+sudo chmod -R 777 /home/ncameiri/data
+sudo chown -R ncameiri:ncameiri /home/ncameiri/data
