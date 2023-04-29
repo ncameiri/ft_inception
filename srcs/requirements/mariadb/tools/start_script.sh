78ea12426cd91596a36aeb19efca3e103086232b
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-29 13:29:05 +0100

    finished

--- srcs/requirements/mariadb/tools/start_script.sh
+++ srcs/requirements/mariadb/tools/start_script.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-service mariadb start 
+service mariadb start 2>/dev/null
 
-    sed -i "s/MYSQL_USER/$MYSQL_USER/g" wp_dump.sql
-    sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" wp_dump.sql
-    sed -i "s/WP_DBNAME/$WP_DBNAME/g" wp_dump.sql
+sed -i "s/MYSQL_USER/$MYSQL_USER/g" wp_dump.sql
+sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" wp_dump.sql
+sed -i "s/WP_DBNAME/$WP_DBNAME/g" wp_dump.sql
 
-SQLQUERY=$(echo "show databases" | mariadb  | grep mydb) 
-#SQLQUERY=$(echo "show databases" | mariadb -u$MYSQL_USER -p$MYSQL_ROOT_PASSWORD  | grep mydb) 
-if [ -z "$SQLQUERY" ] ; then
-    mariadb  < wp_dump.sql
-fi
-echo "DELETE FROM mysql.user WHERE User='root'; FLUSH PRIVILEGES;" | mariadb
-sleep 1
+mariadb <wp_dump.sql 2>/dev/null
+echo "DELETE FROM mysql.user WHERE User='root'; FLUSH PRIVILEGES;" | mariadb 2>/dev/null
+sleep 2
 
 pkill mariadb
-
-mariadbd-safe --bind-address=0.0.0.0 
+sleep 2
+mariadbd-safe --bind-address=0.0.0.0
 
 tail -500f /dev/null
