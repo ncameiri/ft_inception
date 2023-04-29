a541bce66e6b95a1e66976ee11ec19c2b0152fc3
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-29 01:09:43 +0100

    still problems with css

--- srcs/requirements/wordpress/tools/start_script.sh
+++ srcs/requirements/wordpress/tools/start_script.sh
@@ -1,16 +1,19 @@
 echo "###########################"
 echo "STARTED WORDPRESS CONTAINER"
 echo "###########################"
-cp -r /wordpress/* /var/www/wordpress/
-sed -i "s/WP_DBNAME/$WP_DBNAME/g" /var/www/wordpress/wp-config.php
-sed -i "s/MYSQL_USER/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
-sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /var/www/wordpress/wp-config.php
+
+#sed -i "s/WP_DBNAME/$WP_DBNAME/g" /var/www/wordpress/wp-config.php
+#sed -i "s/MYSQL_USER/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
+#sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /var/www/wordpress/wp-config.php
 
 
-chown -R nginx:nginx /var/www/*
-chown -R 755 /var/www/*
+chown -R www-data:www-data /var/www/
+chown -R 775 /var/www/
 #Remove Original config and change for updated
+mkdir -p /run/php
+touch /run/php/php7.4-fpm.pid
 rm /etc/php/7.4/fpm/pool.d/www.conf
-cp /www.conf /etc/php/7.4/fpm/pool.d/
+
+cp /www.conf /etc/php/7.4/fpm/pool.d
 php-fpm7.4 -F -R
 tail -500f /dev/null
